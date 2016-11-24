218f6cf9c1b7afb81bce62356ba45f59ced4c9a6
Harshil Dhariwal <harshil.dhariwal@bitwiseglobal.com>
2016-11-24 12:22:48 +0530

    Added utility to create smaller messages id message length exceeds 320 which is max for fb

---
+++ src/stringUtils.js
@@ -0,0 +1,35 @@
+module.exports.splitResponse = function(str) {
+    if (str.length <= 320) {
+        return [str];
+    }
+
+    return chunkString(str, 300);
+}
+
+function chunkString(s, len) {
+    var curr = len, prev = 0;
+
+    var output = [];
+
+    while (s[curr]) {
+        if (s[curr++] == ' ') {
+            output.push(s.substring(prev, curr));
+            prev = curr;
+            curr += len;
+        }
+        else {
+            var currReverse = curr;
+            do {
+                if (s.substring(currReverse - 1, currReverse) == ' ') {
+                    output.push(s.substring(prev, currReverse));
+                    prev = currReverse;
+                    curr = currReverse + len;
+                    break;
+                }
+                currReverse--;
+            } while (currReverse > prev)
+        }
+    }
+    output.push(s.substr(prev));
+    return output;
+}
