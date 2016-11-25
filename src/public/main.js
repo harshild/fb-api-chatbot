dba351862ab25688d1f9b4c7d8d014eeb6ee978c
Harshil Dhariwal <harshil.dhariwal@gmail.com>
2016-11-25 12:49:08 +0530

    Configured a basic dynamic table

--- src/public/main.js
+++ src/public/main.js
@@ -3,4 +3,28 @@
 angular.module('mainApp', [])
     .controller('mainCtrl', function ($scope, $http) {
         $scope.name = "Harshil";
+
+        $scope.dataRows = [{
+            "Key1" : "Value1",
+            "Key2" : "Value2",
+            "Key3" : "Value3",
+            "Key4" : "Value4",
+            "Key5" : "Value5",
+            "Key6" : "Value6",
+            "Key7" : "Value7"
+        },
+        {
+            "Key1" : "Value11",
+            "Key2" : "Value22",
+            "Key3" : "Value33",
+            "Key4" : "Value44",
+            "Key5" : "Value55",
+            "Key6" : "Value66",
+            "Key7" : "Value77"
+        }
+        ]
+
+
+
+
     });
