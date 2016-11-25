af1d003d03ddbd1c91d0c0b1a5c7aae5eb1dfd93
Harshil Dhariwal <harshil.dhariwal@gmail.com>
2016-11-25 13:04:45 +0530

    made some final changes added refresh data functionality added css

--- src/public/main.js
+++ src/public/main.js
@@ -2,41 +2,25 @@
 
 angular.module('mainApp', [])
     .controller('mainCtrl', function ($scope, $http) {
-        $scope.name = "Harshil";
 
-        $scope.dataRows = [{
-            "Key1": "Value1",
-            "Key2": "Value2",
-            "Key3": "Value3",
-            "Key4": "Value4",
-            "Key5": "Value5",
-            "Key6": "Value6",
-            "Key7": "Value7"
-        },
-            {
-                "Key1": "Value11",
-                "Key2": "Value22",
-                "Key3": "Value33",
-                "Key4": "Value44",
-                "Key5": "Value55",
-                "Key6": "Value66",
-                "Key7": "Value77"
-            }
-        ];
+        $scope.reqInProgress = false;
+        $scope.dataRows = [];
 
         $scope.getTableData = function () {
+            $scope.reqInProgress = true;
             $http({
                 method: 'GET',
                 url: '/getTableData'
             }).
                 success(function (res) {
                     $scope.dataRows = res;
+                    $scope.reqInProgress = false;
                 }).
                 error(function (res) {
                     console.log("Service Not Reachable")
+                    $scope.reqInProgress = false;
                 });
         }
 
-         $scope.getTableData();
-
+        $scope.getTableData();
     });
