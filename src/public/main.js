f5d328894359a6a4dd552dfa2a59b652af9f1dff
Harshil Dhariwal <harshil.dhariwal@gmail.com>
2016-11-25 12:52:21 +0530

    added REST call from angular to retrieve data

--- src/public/main.js
+++ src/public/main.js
@@ -5,26 +5,38 @@ angular.module('mainApp', [])
         $scope.name = "Harshil";
 
         $scope.dataRows = [{
-            "Key1" : "Value1",
-            "Key2" : "Value2",
-            "Key3" : "Value3",
-            "Key4" : "Value4",
-            "Key5" : "Value5",
-            "Key6" : "Value6",
-            "Key7" : "Value7"
+            "Key1": "Value1",
+            "Key2": "Value2",
+            "Key3": "Value3",
+            "Key4": "Value4",
+            "Key5": "Value5",
+            "Key6": "Value6",
+            "Key7": "Value7"
         },
-        {
-            "Key1" : "Value11",
-            "Key2" : "Value22",
-            "Key3" : "Value33",
-            "Key4" : "Value44",
-            "Key5" : "Value55",
-            "Key6" : "Value66",
-            "Key7" : "Value77"
+            {
+                "Key1": "Value11",
+                "Key2": "Value22",
+                "Key3": "Value33",
+                "Key4": "Value44",
+                "Key5": "Value55",
+                "Key6": "Value66",
+                "Key7": "Value77"
+            }
+        ];
+
+        $scope.getTableData = function () {
+            $http({
+                method: 'GET',
+                url: '/getTableData'
+            }).
+                success(function (res) {
+                    $scope.dataRows = res;
+                }).
+                error(function (res) {
+                    console.log("Service Not Reachable")
+                });
         }
-        ]
 
-
-
+         $scope.getTableData();
 
     });
